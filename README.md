8b6c9b7ef9db2fce89fb214f1a85afdbf135442e
Xiphoseer <me@xiphoseer.de>
2023-10-24 10:34:23 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# sysdiagram
---
+++ src/bin/sysdiagram.rs
@@ -0,0 +1,86 @@
+use anyhow::Context;
+use mapr::Mmap;
+use std::fs::File;
+use std::io::Cursor;
+use std::path::PathBuf;
+use sysdiagram::{get_settings, SysDiagram, TryFromCfb};
+
+#[derive(argh::FromArgs)]
+/// parse a sysdiagram from a FDB file
+struct Options {
+    /// path to the FDB file
+    #[argh(positional)]
+    file: PathBuf,
+
+    #[argh(switch)]
+    /// assume the file is base64 encoded
+    base64: bool,
+
+    #[argh(switch)]
+    /// print relationships
+    relationships: bool,
+
+    #[argh(switch)]
+    /// print settings
+    settings: bool,
+
+    #[argh(switch)]
+    /// print dsref
+    dsref: bool,
+
+    #[argh(switch)]
+    /// print tables
+    tables: bool,
+}
+
+fn load_database(opts: &Options) -> Result<(), anyhow::Error> {
+    println!("Loading tables... (this may take a while)");
+
+    // Load the database file
+    let file = File::open(&opts.file)
+        .with_context(|| format!("Failed to open input file '{}'", opts.file.display()))?;
+    let mmap = unsafe { Mmap::map(&file)? };
+    let buf: &[u8] = &mmap;
+    let cursor = Cursor::new(buf);
+
+    if opts.base64 {
+        unimplemented!("--base64 is unimplemented");
+    }
+
+    let sysdiagram = SysDiagram::try_from_cfb(cursor)?;
+    if opts.tables {
+        for table in sysdiagram.tables {
+            println!("{}.{}", table.sch_grid.schema, table.sch_grid.name);
+            eprintln!("{:#?}", table.sch_grid);
+        }
+    }
+    if opts.relationships {
+        for relationship in sysdiagram.relationships {
+            println!(
+                "{:60} {:25} {:25}",
+                relationship.name, relationship.from, relationship.to
+            );
+        }
+    }
+    if opts.settings {
+        if let Ok(settings) = get_settings(sysdiagram.dsref_schema_contents.connection.clone()) {
+            for (key, value) in &settings {
+                println!("{:25}: {}", key, value);
+            }
+        } else {
+            eprintln!(
+                "Failed to parse connection string:\n{}",
+                sysdiagram.dsref_schema_contents.connection
+            );
+        }
+    }
+    if opts.dsref {
+        eprintln!("{:#?}", sysdiagram.dsref_schema_contents);
+    }
+    Ok(())
+}
+
+pub fn main() -> Result<(), anyhow::Error> {
+    let opts: Options = argh::from_env();
+    load_database(&opts).with_context(|| "Loading database failed!")
+}
