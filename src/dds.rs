4cbc16d871b2f399a84d675cde8dac04e329e2e5
Xiphoseer <me@xiphoseer.de>
2023-10-31 09:47:31 +0100

    library docs

--- src/dds.rs
+++ src/dds.rs
@@ -4,8 +4,30 @@
 //! > DDS (DaVinci Design Surface) is an internal name for the design surface we used for our designers.
 //! > It is originally developed by VS and I think it was also used for a few other designers too
 //! > (like the UI designers in VS 6). It is a pretty old design surface and it will soon be replaced everywhere it was used.
-//! 
+//!
 //! See also:
 //! - <https://fjehl.wordpress.com/2010/05/23/ssis-modifier-lapparence-des-packages-via-lapi-fail/>
 //! - <http://sqlsoundings.blogspot.com/2011/10/ssis-word-wrapping-annotations-using.html>
 //! - <https://www.sqlservercentral.com/articles/hidden-ssis-features-word-wrapping-your-annotations-and-more>
+
+use uuid::{uuid, Uuid};
+
+/// Microsoft DT PolyLine Control 2 (ProgID `MSDTPolylineControl.2`)
+pub const CLSID_POLYLINE: Uuid = uuid!("d24d4453-1f01-11d1-8e63-006097d2df48");
+/// Microsoft DT Label Control (ProgID `MSDTDDSLabel.1`)
+pub const CLSID_DDSLABEL: Uuid = uuid!("d24d4451-1f01-11d1-8e63-006097d2df48");
+/// Microsoft DT Diagram Surface 2 (ProgID `MSDTDDS.2`)
+pub const CLSID_MSDTDDS: Uuid = uuid!("b0406340-b0c5-11d0-89a9-00a0c9054129");
+/// Microsoft DT DDS TypeLib 2 (`mdt2dd.dll`)
+pub const TYPELIB_DTDDS2: Uuid = uuid!("b0406341-b0c5-11d0-89a9-00a0c9054129");
+/// Microsoft DT Typelib
+pub const TYPELIB_MSDT: Uuid = uuid!("a92cc3f0-e7c4-11ce-a47b-00aa005119fb");
+
+/// Microsoft DT DDS Form 2.0 (aka `MDTDF.Form.1`)
+pub const CLSID_MSDT_DDS_FORM_2: Uuid = uuid!("105b80d2-95f1-11d0-b0a0-00aa00bdcb5c");
+
+/// Microsoft DT DDSForm (`mdt2df.dll`)
+pub const TYPELIB_DDS_FORM: Uuid = uuid!("105b80d0-95f1-11d0-b0a0-00aa00bdcb5c");
+
+/// Microsoft DT DDSform 2.1 FormPackage
+pub const CLSID_DDS2_FORM_PACKAGE: Uuid = uuid!("105b80d5-95f1-11d0-b0a0-00aa00bdcb5c");
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,24 @@
 //! # Reading MSSQL sysdiagrams
+//!
+//! This crate documents and implements parsers for the binary format used in
+//! the `definition` column of the `[dbo].[sysdiagrams]` system table in
+//! Microsoft SQL Server 2008.
+//!
+//! These represent "Database Diagrams" created in SQL Server Management Studio (SMSS),
+//! or rather its "Visual Database Tools" component. Diagrams from that era encode
+//! a graphical representation of an ER Diagram, that is a set of tables and relations,
+//! but the binary format stores next to no information on the columns themselves.
+//!
+//! Tables are represented by a [`SchGrid`] OLE Control, which is bound to a table of
+//! the Data Source via the [Data Source Reference Object (DSRef)][`dsref`]. In the designer,
+//! each row of this grid control represents a column of a table.
+//!
+//! ## Technical Underpinnings
+//!
+//! The "Microsoft Data Tools Database Designer" alongside its siblings, the "Microsoft Data Tools Query Designer"
+//! and "Microsoft Data Tools Table Designer" were implemented using the "Microsoft DT DDS Form 2.0"
+//! technology. *DDS* in this case stands for [*DaVinci Diagram Surface*][`dds`], which is a set of OLE
+//! Controls originating in Visual Studio.
 
 mod core;
 pub use core::*;
@@ -20,6 +40,8 @@ pub use connection_string::*;
 use dsref::{parse_dsref_schema_contents, DSRefSchemaContents};
 use uuid::{uuid, Uuid};
 
+use crate::dds::{CLSID_DDSLABEL, CLSID_POLYLINE};
+
 const DSREF_SCHEMA_CONTENTS: &str = "/DSREF-SCHEMA-CONTENTS";
 
 // See: http://www.dejadejadeja.com/detech/ocxdb/
@@ -32,6 +54,8 @@ pub const TYPELIB_SCHGRID: Uuid = uuid!("e9b0e6da-811c-11d0-ad51-00a0c90f5739");
 
 /// Microsoft Data Tools Database Designer
 pub const CLSID_MSDTDB_DESIGNER: Uuid = uuid!("e9b0e6d4-811c-11d0-ad51-00a0c90f5739");
+
+pub const PROGID_MSDTDB_DESIGNER: &str = "MSDTDatabaseDesigner2";
 /// Microsoft Data Tools Database Designer SQL Server Table Property Page
 pub const CLSID_MSDTDB_SQLSERVER_TABLE_PROPERTY_PAGE: Uuid =
     uuid!("e9b0e6d5-811c-11d0-ad51-00a0c90f5739");
@@ -50,25 +74,6 @@ pub const IID_CONTROL_EVENTS: Uuid = uuid!("77d2c934-7779-11d8-9070-00065b840d9c
 
 pub const CLSID_ISCHGRID: Uuid = uuid!("b27d32a0-62d8-4295-8d98-273c25a2da2d");
 pub const CLSID_DSCHGRID_EVENTS: Uuid = uuid!("847f3bf4-617f-43c7-8535-2986e1d552f8");
-/// Microsoft DT PolyLine Control 2 (ProgID `MSDTPolylineControl.2`)
-pub const CLSID_POLYLINE: Uuid = uuid!("d24d4453-1f01-11d1-8e63-006097d2df48");
-/// Microsoft DT Label Control (ProgID `MSDTDDSLabel.1`)
-pub const CLSID_DDSLABEL: Uuid = uuid!("d24d4451-1f01-11d1-8e63-006097d2df48");
-/// Microsoft DT Diagram Surface 2 (ProgID `MSDTDDS.2`)
-pub const CLSID_MSDTDDS: Uuid = uuid!("b0406340-b0c5-11d0-89a9-00a0c9054129");
-/// Microsoft DT DDS TypeLib 2 (`mdt2dd.dll`)
-pub const TYPELIB_DTDDS2: Uuid = uuid!("b0406341-b0c5-11d0-89a9-00a0c9054129");
-/// Microsoft DT Typelib
-pub const TYPELIB_MSDT: Uuid = uuid!("a92cc3f0-e7c4-11ce-a47b-00aa005119fb");
-
-/// Microsoft DT DDS Form 2.0 (aka `MDTDF.Form.1`)
-pub const CLSID_MSDT_DDS_FORM_2: Uuid = uuid!("105b80d2-95f1-11d0-b0a0-00aa00bdcb5c");
-
-/// Microsoft DT DDSForm (`mdt2df.dll`)
-pub const TYPELIB_DDS_FORM: Uuid = uuid!("105b80d0-95f1-11d0-b0a0-00aa00bdcb5c");
-
-/// Microsoft DT DDSform 2.1 FormPackage
-pub const CLSID_DDS2_FORM_PACKAGE: Uuid = uuid!("105b80d5-95f1-11d0-b0a0-00aa00bdcb5c");
 
 pub struct SysDiagramFile<T> {
     inner: OFormsFile<T>,
